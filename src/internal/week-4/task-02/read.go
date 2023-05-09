f7c67bd7e6c3148d221d4166168ad1f71e905100
Sebastian Sigl <sebastian.sigl@adevinta.com>
2023-05-09 09:05:29 +0200

    Revert week 4 task 2

--- src/internal/week-4/task-02/read.go
+++ src/internal/week-4/task-02/read.go
@@ -7,45 +7,72 @@ import (
 	"strings"
 )
 
-func main() {
-
-	var fileName string
-	//var err error
-
-	//fileName = "/home/juaneco/jcfr/desarrollo/go/input.txt"
-
-	fmt.Printf("\nEnter a file name: ")
-	_, _ = fmt.Scan(&fileName)
-
-	file, _ := os.Open(fileName)
-
-	scanner := bufio.NewScanner(file)
-
-	var persons = make([]PersonStruct, 0)
-
-	for scanner.Scan() {
-
-		line := scanner.Text()
-		lineSplit := strings.Split(line, " ")
-
-		// creating a new person
-		person := PersonStruct{
-			fname: lineSplit[0],
-			lname: lineSplit[1],
-		}
-
-		// append person to the slice persons
-		persons = append(persons, person)
-	}
-
-	_ = file.Close()
-
-	for _, person := range persons {
-		fmt.Printf("%s %s\n", person.fname, person.lname)
-	}
-}
-
-type PersonStruct struct {
+type person struct {
 	fname string
 	lname string
+}
+
+func (p person) String() string {
+	return fmt.Sprintf(`
+		first name:			%s
+		last name:			%s`, p.fname, p.lname)
+}
+
+func main() {
+	filePath := requestFilePath()
+
+	file := mustOpen(filePath)
+	defer file.Close()
+
+	persons := scanPersonsFromFile(file)
+
+	printSummary(persons)
+}
+
+func requestFilePath() string {
+	fmt.Print("enter a path to a file to parse:")
+
+	var filePath string
+	_, err := fmt.Scan(&filePath)
+	if err != nil {
+		panic(err)
+	}
+
+	return filePath
+}
+
+func mustOpen(filePath string) *os.File {
+	file, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+
+	return file
+}
+
+func scanPersonsFromFile(file *os.File) []person {
+	scanner := bufio.NewScanner(file)
+
+	persons := []person{}
+	for scanner.Scan() {
+		lineText := scanner.Text()
+		lineSplit := strings.Split(lineText, " ")
+		if len(lineSplit) != 2 {
+			panic(fmt.Sprintf("Can not parse line '%v'", lineText))
+		}
+
+		persons = append(persons, person{
+			fname: lineSplit[0],
+			lname: lineSplit[1],
+		})
+	}
+
+	return persons
+}
+
+func printSummary(persons []person) {
+	fmt.Printf("Found %v persons:", len(persons))
+	for _, person := range persons {
+		fmt.Println(person)
+	}
 }
